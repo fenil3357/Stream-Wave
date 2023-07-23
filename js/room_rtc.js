f3778da4785a1ccfae5d160a1ab21bf4d46fba2b
Fenil <fenil3357@gmail.com>
2023-07-23 09:53:21 +0530

    Displayframe events and handlers

--- js/room_rtc.js
+++ js/room_rtc.js
@@ -36,13 +36,22 @@ let joinRoomInit = async () => {
 }
 
 let joinStream = async () => {
-    localTrack = await AgoraRTC.createMicrophoneAndCameraTracks();
+    localTrack = await AgoraRTC.createMicrophoneAndCameraTracks(
+        // {},
+        // {
+        //     encoderConfig: {
+        //         width: { min: 640, ideal: 1920, max: 1920 },
+        //         height: { min: 480, ideal: 1080, max: 1080 }
+        //     }
+        // }
+    );
 
     let player = `<div class="video__container" id="user-container-${uid}">
                     <div class="video-player" id="user-${uid}"></div> 
                   </div>`
 
     document.getElementById('streams__container').insertAdjacentHTML('beforeend', player);
+    document.getElementById(`user-container-${uid}`).addEventListener('click', expandVideoFrame);
 
     localTrack[1].play(`user-${uid}`)
     await client.publish([localTrack[0], localTrack[1]]);
@@ -53,7 +62,7 @@ let handleUserPublished = async (user, mediaType) => {
 
     await client.subscribe(user, mediaType);
 
-    let player = document.getElementById('user-container-${user.uid}')
+    let player = document.getElementById(`user-container-${user.uid}`)
 
     if (player === null) {
         player = `<div class="video__container" id="user-container-${user.uid}">
@@ -61,13 +70,20 @@ let handleUserPublished = async (user, mediaType) => {
                   </div>`
 
         document.getElementById('streams__container').insertAdjacentHTML('beforeend', player);
+        document.getElementById(`user-container-${user.uid}`).addEventListener('click', expandVideoFrame);
     }
 
-    if(mediaType === 'video') {
+    // If a user is currently on focus and some other user join, then make new user's videoFrame small
+    if (displayFrame.style.display) {
+        player.style.height = '100px';
+        player.style.width = '100px';
+    }
+
+    if (mediaType === 'video') {
         user.videoTrack.play(`user-${user.uid}`);
     }
 
-    if(mediaType === 'audio') {
+    if (mediaType === 'audio') {
         user.audioTrack.play();
     }
 }
@@ -75,6 +91,18 @@ let handleUserPublished = async (user, mediaType) => {
 let handleUserLeft = async (user) => {
     delete remoteUsers[user.uid]
     document.getElementById(`user-container-${user.uid}`).remove();
+
+    // If the user who left was currently on focus(mainFrame)
+    if (userIdInDisplayFrame == `user-container-${user.uid}`) {
+        displayFrame.style.display = null;
+
+        let videoFrames = document.getElementsByClassName('video__container')
+
+        for (let i = 0; i<videoFrames.length; i++) {
+            videoFrames[i].style.height = '300px'
+            videoFrames[i].style.width = '300px'
+        }
+    }
 }
 
 joinRoomInit();
